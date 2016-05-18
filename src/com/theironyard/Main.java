4ee6e37e8fe5b33fa24cfc1b5be3be63ea31b690
halfshelldell <simmonslydell@gmai.com>
2016-05-18 11:31:44 -0400

    added exceptions/start adventure

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,45 @@
 package com.theironyard;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome traveller!");
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        System.out.println("Welcome, " + name);
+
+        System.out.println("Choose your weapon [sword/mace]");
+
+        String weapon = scanner.nextLine();
+        //System.out.println("You choose the, " + weapon);
+
+        if (weapon.equalsIgnoreCase("sword")) {
+            System.out.println("Sword is a fine choice");
+        }
+        else if (weapon.equalsIgnoreCase("mace")) {
+            System.out.println("Mace is a fine choice");
+        }
+        else {
+            throw new Exception("Invalid weapon!");
+        }
+
+        System.out.println("Choose your location [forrest/tunnel]");
+
+        String location = scanner.nextLine();
+
+        if (location.equalsIgnoreCase("forrest")) {
+            System.out.println("Entering forrest...");
+        }
+        else if (location.equalsIgnoreCase("tunnel")) {
+            System.out.println("Entering tunnel...");
+        }
+        else {
+            throw new Exception("Invalid location!");
+        }
     }
 }
